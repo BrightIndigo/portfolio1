072c6a63d6b43cfc58eaee0882612cdb81b1c530
BrightIndigo <128691376+BrightIndigo@users.noreply.github.com>
2023-05-14 23:05:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function galeria() {
+  window.open("galeria.html", "_self");
+}
+
+function kontakt() {
+  window.open("kontakt.html", "_self");
+}
+
+function autor() {
+  window.open("autor.html", "_self");
+}
